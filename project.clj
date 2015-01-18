5c1fffdc5cad20e590226d05a9b02a5496ddce06
John Jacobsen <john@mail.npxdesigns.com>
2015-01-18 08:46:27 -0600

    Add TravisCI integration.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject trav/trav "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [eigenhombre/namejen "0.1.8"]
-                 [midje "1.6.3"]]
+                 [eigenhombre/namejen "0.1.8"]]
   :aliases {"autotest" ["midje" ":autotest"]}
   :main trav.core
-  :description "Riffing off Classic Traveller, a popular 80s-era SF RPG")
+  :description "Riffing off Classic Traveller, a popular 80s-era SF RPG"
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}
+             :uberjar {:aot :all}})
