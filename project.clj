82bd3e36fa0cd6c616fb424cc44b3b03fe7303d9
John Jacobsen <jacobsen@airrrrr.local>
2017-07-27 20:16:34 -0500

    Update Clojure version and (partially) yank Midje.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject trav/trav "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [eigenhombre/namejen "0.1.8"]]
   :aliases {"autotest" ["midje" ":autotest"]}
   :main trav.core
   :description "Riffing off Classic Traveller, a popular 80s-era SF RPG"
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}
-             :uberjar {:aot :all}})
+  :target-path "target/"
+  :uberjar-name "trav.jar"
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]}})
+
